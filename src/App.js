0b779c8aadccb8da8dd48e72ef06fe521c0e6c5e
Jean Khoge <khogejean@gmail.com>
2023-09-02 02:13:09 +0200

    fixe description final

--- src/App.js
+++ src/App.js
@@ -62,12 +62,9 @@ function App() {
         <meta name='keywords' content='ip address, location, timezone, isp' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='./assets/ip-address-svgrepo-com.svg' />
-        <meta property="og:image" content="./assets/app-view.png"></meta>
+        <meta property="image" content="./assets/app-view.png"></meta>
         <meta charset='utf-8' />
         <meta name='theme-color' content='#000000' />
-        <meta name='description' content='Web site created using create-react-app' />
-        <link rel='apple-touch-icon' href='%PUBLIC_URL%/logo192.png' />
-        <link rel='manifest' href='%PUBLIC_URL%/manifest.json' />
         <title>IP Address Tracker</title>
       </Helmet>
         <div className='content'>
